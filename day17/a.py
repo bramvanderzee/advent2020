cde2c0b42a2a61446f1315a5ad8cb4a1480b741e
bramvanderzee <bram.vdzee@gmail.com>
2020-12-17 16:25:13 +0100

    still working on it

--- day17/a.py
+++ day17/a.py
@@ -1,3 +1,4 @@
+C = {}
 def to_dict(cubes):
     dict_ = {}
     for iz,z in enumerate(cubes):
@@ -6,23 +7,24 @@ def to_dict(cubes):
                 dict_[(ix, iy, iz)] = x
     return dict_
 
-def get_num_nearby(cubes: dict, coord):
+def get_num_nearby_expanding(coord):
     num = 0
     x, y, z = coord
     for dx in [-1, 0, 1]:
         for dy in [-1, 0, 1]:
             for dz in [-1, 0, 1]:
-                if cubes.get((x+dx, y+dy, z+dz)) == '#':
+                c = C.get((x+dx, y+dy, z+dz))
+                if c == '#':
                     num += 1
+                elif c == None:
+                    C[(x+dx, y+dy, z+dz)] = '.'
     return num
 
-def expand_cube(cubes: dict):
-    max_
-
-def do_cycle(cubes: dict):
+def do_cycle():
     new_cubes = {}
-    for k,v in cubes.items():
-        num = get_num_nearby(cubes,k)
+    for k in list(C.keys()):
+        num = get_num_nearby_expanding(k)
+        v = C[k]
         if v == '#':
             if 2 <= num <= 3:
                 new_cubes[k] = '#'
@@ -35,8 +37,8 @@ def do_cycle(cubes: dict):
                 new_cubes[k] = '.'
     return new_cubes
 
-def count_active(cube_dict: dict) -> int:
-    num = list(cube_dict.values()).count('#')
+def count_active() -> int:
+    num = list(C.values()).count('#')
     return num
 
 cubes = []
@@ -46,10 +48,10 @@ with open('input_ex.txt') as f:
         y.append([c for c in x.strip()])
     cubes.append(y)
 
-cubes = to_dict(cubes)
-print(cubes)
-for cycle in range(6):
-    cubes = do_cycle(expand_cube(cubes))
+C = to_dict(cubes)
 
-print(get_num_nearby(cubes, (2, 2, 0)))
-print(count_active(cubes))
+for cycle in range(6):
+    C = do_cycle()
+    print(C)
+
+print(count_active())
