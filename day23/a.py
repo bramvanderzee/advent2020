9e84c5d321610445965b5bf8f506ed381ddb268a
bramvanderzee <bram.vdzee@gmail.com>
2020-12-23 14:11:19 +0100

    Tried optimizing

--- day23/a.py
+++ day23/a.py
@@ -1,16 +1,16 @@
-cups = []
+CUPS = []
 with open('input_ex.txt') as f:
-    cups = [int(x) for x in f.readline().strip()]
+    CUPS = [int(x) for x in f.readline().strip()]
 
-MIN = min(cups)
-MAX = max(cups)
+MIN = min(CUPS)
+MAX = max(CUPS)
 
-def do_move(all_cups: list, index: int) -> list:
-    cur_cup = all_cups[index]
-    indices = [(index+x)%len(all_cups) for x in range(1,4)]
-    picked = [all_cups[x] for x in indices]
+def do_move(index: int) -> list:
+    cur_cup = CUPS[index]
+    indices = [(index+x)%len(CUPS) for x in range(1,4)]
+    picked = [CUPS[x] for x in indices]
     for cup in picked:
-        all_cups.remove(cup)
+        CUPS.remove(cup)
     dest_cup = cur_cup - 1 if not cur_cup == MIN else MAX
     
     while dest_cup in picked:
@@ -18,21 +18,20 @@ def do_move(all_cups: list, index: int) -> list:
         if dest_cup < MIN:
             dest_cup = MAX
     
-    dest_cup_i = all_cups.index(dest_cup) + 1
+    dest_cup_i = CUPS.index(dest_cup) + 1
     for cup in picked[::-1]:
-        all_cups.insert(dest_cup_i, cup)
-    return all_cups 
+        CUPS.insert(dest_cup_i, cup)
 
-def print_cups(cups: list) -> None:
-    index = cups.index(1)
-    new_list = [str(x) for x in cups[index+1:]]
-    new_list.extend([str(x) for x in cups[:index]])
+def print_cups() -> None:
+    index = CUPS.index(1)
+    new_list = [str(x) for x in CUPS[index+1:]]
+    new_list.extend([str(x) for x in CUPS[:index]])
     print(''.join(new_list))
 
 cur_cup_i = 0
 for move in range(1, 101):
-    prev = cups[cur_cup_i]
-    cups = do_move(cups, cur_cup_i)
-    cur_cup_i = (cups.index(prev)+1)%len(cups)
+    prev = CUPS[cur_cup_i]
+    do_move(cur_cup_i)
+    cur_cup_i = (CUPS.index(prev)+1)%len(CUPS)
 
-print_cups(cups)
+print_cups()
--- day23/b.py
+++ day23/b.py
@@ -1,18 +1,18 @@
-cups = []
-with open('input_ex.txt') as f:
-    cups = [int(x) for x in f.readline().strip()]
+CUPS = [] 
+with open('input.txt') as f:
+    CUPS = [int(x) for x in f.readline().strip()]
 
-from_cup = max(cups)+1
-cups.extend([x+from_cup for x in range(1000000-len(cups))])
-MIN = min(cups)
-MAX = max(cups)
+from_cup = max(CUPS)+1
+CUPS.extend([x+from_cup for x in range(1000000-len(CUPS))])
+MIN = min(CUPS)
+MAX = max(CUPS)
 
-def do_move(all_cups: list, index: int) -> list:
-    cur_cup = all_cups[index]
-    indices = [(index+x)%len(all_cups) for x in range(1,4)]
-    picked = [all_cups[x] for x in indices]
+def do_move(index: int):
+    cur_cup = CUPS[index]
+    indices = [(index+x)%len(CUPS) for x in range(1,4)]
+    picked = [CUPS[x] for x in indices]
     for cup in picked:
-        all_cups.remove(cup)
+        CUPS.remove(cup)
     dest_cup = cur_cup - 1 if not cur_cup == MIN else MAX
     
     while dest_cup in picked:
@@ -20,21 +20,20 @@ def do_move(all_cups: list, index: int) -> list:
         if dest_cup < MIN:
             dest_cup = MAX
     
-    dest_cup_i = all_cups.index(dest_cup) + 1
+    dest_cup_i = CUPS.index(dest_cup) + 1
     for cup in picked[::-1]:
-        all_cups.insert(dest_cup_i, cup)
-    return all_cups 
+        CUPS.insert(dest_cup_i, cup)
 
 cur_cup_i = 0
 for move in range(1, 10000001):
-    prev = cups[cur_cup_i]
-    cups = do_move(cups, cur_cup_i)
-    cur_cup_i = (cups.index(prev)+1)%len(cups)
+    prev = CUPS[cur_cup_i]
+    do_move(cur_cup_i)
+    cur_cup_i = (CUPS.index(prev)+1)%len(CUPS)
     if move%10000 == 0:
         print(move)
 
-index1 = cups.index(1)
-cup1, cup2 = cups[(index1+1)%len(cups)], cups[(index1+2)%len(cups)]
+index1 = CUPS.index(1)
+cup1, cup2 = CUPS[(index1+1)%len(CUPS)], CUPS[(index1+2)%len(CUPS)]
 ans = cup1 * cup2
 print()
 print(cup1, cup2)
