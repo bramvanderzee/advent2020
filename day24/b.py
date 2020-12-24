28494060e47a25127d37cd4b93876c916dc4e35d
bramvanderzee <bram.vdzee@gmail.com>
2020-12-24 15:03:20 +0100

    got b working

--- day24/b.py
+++ day24/b.py
@@ -2,7 +2,7 @@ from collections import defaultdict, deque
 MAP = defaultdict(lambda: False)
 # Black = True, White = False
 dirs = {'e': (2,0), 'ne': (1,1), 'nw': (-1,1), 'w': (-2,0), 'sw': (-1,-1), 'se': (1,-1)}
-with open('input_ex.txt') as f:
+with open('input.txt') as f:
     for x in f:
         x = x.strip()
         route = deque()
@@ -18,25 +18,33 @@ with open('input_ex.txt') as f:
             x, y = x+dx, y+dy
         MAP[(x, y)] = not MAP[(x, y)]
 
+def get_num_neighbours(M, coord):
+    global dirs
+    num = 0
+    for dx, dy in dirs.values():
+        x, y = coord
+        if M[(x+dx,y+dy)] == True:
+            num += 1
+    return num
+
 print(list(MAP.values()).count(True))
 for day in range(100):
     new_map = {}
-    nums = defaultdict(int)
+    nums = {}
 
     for key in list(MAP.keys()):
-        nums[key] = 0
+        nums[key] = get_num_neighbours(MAP, key)
         for dx, dy in dirs.values():
             x, y = key
-            if MAP[(x+dx,y+dy)] == True:
-                nums[key] += 1
+            coord = (x+dx, y+dy)
+            if not coord in nums.keys():
+                nums[coord] = get_num_neighbours(MAP, coord)
 
-    to_check = deque([x for x in MAP.keys()])
-    while len(to_check) > 0:
-        key = to_check.popleft()
+    for key in list(nums.keys()):
         if MAP[key] == True and (nums[key] == 0 or nums[key] > 2):
             new_map[key] = False
         elif MAP[key] == False and nums[key] == 2:
             new_map[key] = True
     for key,value in new_map.items():
         MAP[key] = value 
-    print(day+1, list(MAP.values()).count(True))
+print(list(MAP.values()).count(True))
